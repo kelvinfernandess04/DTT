437d13beca58fae843ed4d62c3f3f715defd405b
Kelvin <kelvin.fernandess04@hotmail.com>
2024-04-24 14:49:50 -0300

    Dockerfile adicionado, mas não funcional. Comentário na SQLQuery para exeplicação de como criar o projeto em outra máquina

--- nodejsApp/SQLQuery_1.sql
+++ nodejsApp/SQLQuery_1.sql
@@ -1,3 +1,8 @@
+-- Arquivo contendo os comandos para a criação do banco de dados.
+-- Caso queira testar a aplicação, use estes comandos, coloque a
+-- string de conexão no app.js, declare a variável de ambiente 
+-- com a senha do banco e a chame no seu projeto
+
 -- CREATE DATABASE DTT
 USE DTT
 -- CREATE TABLE Produtos (
@@ -6,10 +11,10 @@ USE DTT
 --     Descricao NVARCHAR(255) NOT NULL
 -- );
 
-INSERT INTO Produtos (Preco, Descricao)
-VALUES (19.99, 'Produto Exemplo 1'),
-       (29.99, 'Produto Exemplo 2'),
-       (39.99, 'Produto Exemplo 3');
+-- INSERT INTO Produtos (Preco, Descricao)
+-- VALUES (19.99, 'Produto Exemplo 1'),
+--        (29.99, 'Produto Exemplo 2'),
+--        (39.99, 'Produto Exemplo 3');
 
 
 SELECT * FROM Produtos
